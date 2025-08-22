76553b2295735dd74314a23fe042f6d414f43c40
SergioBejarano <sergio.bejarano-r@mail.escuelaing.edu.co>
2025-08-22 08:44:40 -0500

    feat: parte I con 3 hilos

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,30 @@
 package edu.eci.arsw.primefinder;
 
+/**
+ * Main program for concurrent prime number calculation.
+ *
+ * @author sergio.bejarano-r
+ * @author laura.rsanchez
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
-	}
-	
+    /**
+     * Main method that splits the task into three concurrent threads.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        
+        int start = 0;
+        int end = 30000000;
+        int range = (end - start) / 3;  
+
+        PrimeFinderThread pft1 = new PrimeFinderThread(start, start + range);
+        PrimeFinderThread pft2 = new PrimeFinderThread(start + range + 1, start + 2 * range);
+        PrimeFinderThread pft3 = new PrimeFinderThread(start + 2 * range + 1, end);
+A
+        pft1.start();
+        pft2.start();
+        pft3.start();
+    }
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,6 +3,13 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
+
+/**
+ * Thread implementation that finds all prime numbers within a given range.
+ *
+ * @author sergio.bejarano-r
+ * @author laura.rsanchez
+ */
 public class PrimeFinderThread extends Thread{
 
 	
@@ -16,6 +23,14 @@ public class PrimeFinderThread extends Thread{
 		this.b = b;
 	}
 
+	/**
+     * Executes the prime number search in the given interval.
+     * <p>
+     * Every prime number found is added to {@link #primes} and also
+     * printed to the standard output.
+     * </p>
+     */
+    @Override
 	public void run(){
 		for (int i=a;i<=b;i++){						
 			if (isPrime(i)){
@@ -27,6 +42,12 @@ public class PrimeFinderThread extends Thread{
 		
 	}
 	
+	/**
+     * Checks whether a given number is prime.
+     *
+     * @param n the number to check
+     * @return {@code true} if {@code n} is prime, {@code false} otherwise
+     */
 	boolean isPrime(int n) {
 	    if (n%2==0) return false;
 	    for(int i=3;i*i<=n;i+=2) {
@@ -36,6 +57,11 @@ public class PrimeFinderThread extends Thread{
 	    return true;
 	}
 
+	/**
+     * Returns the list of prime numbers found by this thread.
+     *
+     * @return a list containing the discovered prime numbers
+     */
 	public List<Integer> getPrimes() {
 		return primes;
 	}
