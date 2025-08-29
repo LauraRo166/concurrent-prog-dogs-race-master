0eaaed1885d02f960ab4e7c04ed457e49ed67a62
SergioBejarano <sergio2018.bejarano>
2025-08-28 22:22:02 -0500

    refactor: bucle pausa y reanudar en hilos - primos

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -18,10 +18,10 @@ public class Main {
      * @param args command-line arguments (not used)
      */
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+
         int start = 0;
         int end = 30000000;
-        int range = (end - start) / 3;  
+        int range = (end - start) / 3;
 
         PrimeFinderThread pft1 = new PrimeFinderThread(start, start + range);
         PrimeFinderThread pft2 = new PrimeFinderThread(start + range + 1, start + 2 * range);
@@ -31,36 +31,37 @@ public class Main {
         pft2.start();
         pft3.start();
 
-        Thread.sleep(5000);
-
-        pft1.pauseThread();
-        pft2.pauseThread();
-        pft3.pauseThread();
-
-        int totalPrimes = pft1.getPrimes().size()
-                        + pft2.getPrimes().size()
-                        + pft3.getPrimes().size();
-
-        System.out.println("\n==== Ejecución pausada ====");
-        System.out.println("Primos encontrados hasta ahora: " + totalPrimes);
-        System.out.println("Presione ENTER para continuar...");
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
 
-        pft1.resumeThread();
-        pft2.resumeThread();
-        pft3.resumeThread();
+        while (pft1.isAlive() || pft2.isAlive() || pft3.isAlive()) {
+            Thread.sleep(5000);
 
+            pft1.pauseThread();
+            pft2.pauseThread();
+            pft3.pauseThread();
+
+            int totalPrimes = pft1.getPrimes().size()
+                    + pft2.getPrimes().size()
+                    + pft3.getPrimes().size();
+
+            System.out.println("\n==== Ejecución pausada ====");
+            System.out.println("Primos encontrados hasta ahora: " + totalPrimes);
+            System.out.println("Presione ENTER para continuar...");
+
+            br.readLine();
+
+            pft1.resumeThread();
+            pft2.resumeThread();
+            pft3.resumeThread();
+        }
         pft1.join();
         pft2.join();
         pft3.join();
 
         System.out.println("\n==== Ejecución terminada ====");
-        System.out.println("Total de primos encontrados: " + (
-                pft1.getPrimes().size() +
+        System.out.println("Total de primos encontrados: " + (pft1.getPrimes().size() +
                 pft2.getPrimes().size() +
                 pft3.getPrimes().size()));
-    
+
     }
 }
